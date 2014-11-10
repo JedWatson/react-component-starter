d4f87f34e382176ad82695b4cc7c6e876991eeab
Jed Watson <jed.watson@me.com>
2014-11-10 14:47:26 +1100

    Example component src

---
+++ src/Component.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var ExampleComponent = React.createClass({
+	
+	render: function() {
+		return <div>My Component</div>;
+	}
+	
+});
+
+module.exports = ExampleComponent;
